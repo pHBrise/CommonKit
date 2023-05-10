452d5d9356661217964fd74793cb22d95db486d7
Phanvit <phanvit@rs.co.th>
2023-05-10 15:30:18 +0700

    fix code

--- Sources/CommonKit/BaseViewController.swift
+++ Sources/CommonKit/BaseViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-public class BaseViewController: UIViewController {
+open class BaseViewController: UIViewController {
     var logoImageView: UIImageView!
     var navigationBarItems: UIStackView?
 
